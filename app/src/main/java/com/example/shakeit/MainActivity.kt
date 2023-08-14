7f8e8239990ebcc9aa6f5267642ddf8d4255e98d
Dmitry <pavlovdmitry7991@gmail.com>
2023-08-14 21:19:58 +0300

    Added Onboarding screens.

--- app/src/main/java/com/example/shakeit/MainActivity.kt
+++ app/src/main/java/com/example/shakeit/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.shakeit
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.shakeit.ui.allcocktails.AllCocktailsFragment
+import com.example.shakeit.ui.onboarding.OnboardingFragment
+import com.example.shakeit.util.replaceFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +13,7 @@ class MainActivity : AppCompatActivity() {
         Thread.sleep(3000)
         installSplashScreen()
         setContentView(R.layout.activity_main)
+        supportFragmentManager
+            .replaceFragment(R.id.container, OnboardingFragment())
     }
 }
